125ec71fb2ff275ac067a68658281f4fc2a223cc
epiii <gue.elfrianto@gmail.com>
2016-10-17 00:44:03 +0700

    ok

---
+++ 1. pemrograman dasar/03 expression & iinput output/C. Burung Beo/3c.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	char kal[100];
+	cout<<"masukkan kalimat : ";
+		cin>>kal;
+	cout<<"jumlah hurug : "<<strlen(kal);
+	cout<<"cetak kalimat : "<<kal;
+	return 0;
+}
