1c7d827b9136812104e1cd0b5c15bfafdcdc8d46
epiii <gue.elfrianto@gmail.com>
2016-10-22 19:57:39 +0700

    ok

--- 1. pemrograman dasar/06 Perulangan/C. While (done)/6c.cpp
+++ 1. pemrograman dasar/06 Perulangan/C. While (done)/6c.cpp
@@ -2,20 +2,46 @@
 #include <string> 
 using namespace std;
 
+bool cekHuruf(string str){
+	bool result=true;
+	for(int i = 0; i < str.length(); ++i) {
+		if(!isalpha(str[i])){
+			result=false;
+			break;
+		}
+	}return result;
+}
+
 int main(){
-	int counter=0;
-	string kal;
-	string kalArr[100];
+	int counter=1, 
+		maxLoop=100, 	//	ubah sesuai selera 
+		maxChar=100;	// 	ubah sesuai selera
+	bool lanjutkan;
+	string allkata="";
+	cout<<"# : untuk berhenti\n";
+	
 	do{
-		cout<<"masukkan kalimat  :";
-		cin>>kal;
-
-		kalArr[counter]=kal;
-		counter++;
-	}while(kal!="N");
-
-	for (int i = 0; i < counter; ++i){
-		cout<<"\n"<<kalArr[i]<<endl;
-	}
+		string kata;
+		cout<<"kata ke-"<<counter<<" :";
+		cin>>kata;
+		
+		if(kata=="#") { // untuk berhenti input 
+			cout<<">> Anda memutuskan untuk berhenti \n";
+			lanjutkan=false;
+		}else if (counter>maxLoop){ // melebihi batas kata 
+			cout<<">> Maksimal "<<maxLoop<<" kata  !!\n";
+			lanjutkan=false;
+		}else if(kata.length()>maxChar) // melebihi batas karakter  
+			cout<<">> Maksimal  "<<maxChar<<" karakter !!\n";
+		else if(!cekHuruf(kata)) // selain huruf  
+			cout<<">> hanya huruf (a-z)) !!\n";
+		else { // normal
+			allkata+=kata+"\n";
+			counter++;
+		}
+	} while (lanjutkan);
+	cout<<"\ncetak :\n"<<(counter==1?"Anda belum memasukkan kata":allkata);
 	return 0;
 }
+
+// reference :  https://cal-linux.com/tutorials/strings.html
