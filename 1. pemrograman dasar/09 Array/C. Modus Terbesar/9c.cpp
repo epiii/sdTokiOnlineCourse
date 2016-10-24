4a309ccbdb2857522418623dafaf4cff0e1dfaea
epiii <gue.elfrianto@gmail.com>
2016-10-24 07:55:51 +0700

    ok

--- 1. pemrograman dasar/09 Array/C. Modus Terbesar/9c.cpp
+++ 1. pemrograman dasar/09 Array/C. Modus Terbesar/9c.cpp
@@ -9,5 +9,11 @@ int main (){
 	for (int i = 0; i < jum; ++i){
 		cin>>bil[i];
 	}
+
+	int modus[10];
+	for (int i = 0; i < jum; ++i){
+		modus[i]
+	}
+	cout<<"modus:"<<bil[0];
 	return 0;
 }
---
+++ 1. pemrograman dasar/09 Array/D. Rotasi Matriks (done)/9d.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+int main(){
+	int m,n;
+	cout<<"masukkan ordo m x n :";
+	cin>>m>>n;
+
+	int mat[100][100];
+	for (int i = 0; i < m; i++){
+		for(int j=0; j<n; j++){
+			cin>>mat[i][j];
+		}
+	}cout<<"\ncetak:\n"; // m x n = 4 x 3 
+	for (int i = 0; i<=n-1; i++){ 
+		for(int j=m-1; j>=0; j--){
+			cout<<mat[j][i]<<" ";
+		}cout<<endl;
+	}return 0;
+}
+/*
+m x n
+4 x 3
+
+1 2 3
+4 5 6
+7 8 9
+10 11 12
+
+10 7 4 1
+11 8 5 2
+12 9 6 3
+
+30 20 10 00
+31 21 11 01 
+32 22 12 02 
+
+*/
